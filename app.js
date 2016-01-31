94f0ad4247ef0e479c746670da5b42d0498d123f
Eric Stephenson <ericb.stephenson@gmail.com>
2016-01-31 14:24:55 -0800

    first commit, routing and multiple views

---
+++ app.js
@@ -0,0 +1,91 @@
+Parties = new Mongo.Collection("parties");
+
+if (Meteor.isClient) {
+  angular.module('socially', ['angular-meteor', 'ui.router']);
+
+  angular.module('socially').config(function ($urlRouterProvider, $stateProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+ 
+    $stateProvider
+      .state('parties', {
+        url: '/parties',
+        template: '<parties-list></parties-list>'
+      })
+      .state('partyDetails', {
+      	url: '/parties/:partyId',
+      	template: '<party-details></party-details>'
+      });
+ 
+    $urlRouterProvider.otherwise("/parties");
+  });
+
+  angular.module('socially').directive('partiesList', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'parties-list.html',
+      controllerAs: 'partiesList',
+      controller: function($scope, $reactive) {
+      	$reactive(this).attach($scope);
+
+      	this.newParty = {};
+
+        this.helpers({
+          parties: () => {
+          	return Parties.find({});
+          }
+        });
+
+        this.addParty = () => {
+          Parties.insert(this.newParty);
+          this.newParty = {};
+        };
+
+        this.removeParty = (party) => {
+          Parties.remove({_id: party._id});
+        }
+      }
+    }
+  });
+
+  angular.module('socially').directive('partyDetails', function() {
+  	return {
+  	  restrict: 'E',
+  	  templateUrl: 'party-details.html',
+  	  controllerAs: 'partyDetails',
+  	  controller: function($scope, $stateParams) {
+  	  	this.partyId = $stateParams.partyId;
+  	  }
+  	}
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
